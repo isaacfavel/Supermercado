4147660cb0894afb6f0cc2897a6899ae0eaaf821
isaacfavel <pirrisfavela@gmail.com>
2025-10-14 15:53:18 -0600

    se agrga cliente

--- Supermercado/Form1.Designer.cs
+++ Supermercado/Form1.Designer.cs
@@ -28,87 +28,272 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.tbc = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button2 = new System.Windows.Forms.Button();
-            this.tabControl1.SuspendLayout();
+            this.txtNombreCliente = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtApellidoCliente = new System.Windows.Forms.TextBox();
+            this.txtTipoDocCliente = new System.Windows.Forms.TextBox();
+            this.txtNumeroDocCliente = new System.Windows.Forms.TextBox();
+            this.txtTelefonoPCliente = new System.Windows.Forms.TextBox();
+            this.txtTelefonoSCliente = new System.Windows.Forms.TextBox();
+            this.txtCorreoCliente = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.tbc.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnClientes
             // 
-            this.button1.Location = new System.Drawing.Point(343, 131);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnClientes.Location = new System.Drawing.Point(44, 35);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(75, 23);
+            this.btnClientes.TabIndex = 1;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.UseVisualStyleBackColor = true;
+            this.btnClientes.Click += new System.EventHandler(this.button1_Click);
             // 
-            // tabControl1
+            // tbc
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(255, 212);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(500, 307);
-            this.tabControl1.TabIndex = 2;
+            this.tbc.Controls.Add(this.tabPage1);
+            this.tbc.Controls.Add(this.tabPage2);
+            this.tbc.Location = new System.Drawing.Point(12, 109);
+            this.tbc.Name = "tbc";
+            this.tbc.SelectedIndex = 0;
+            this.tbc.Size = new System.Drawing.Size(1107, 537);
+            this.tbc.TabIndex = 2;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.label8);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(492, 278);
+            this.tabPage1.Size = new System.Drawing.Size(1099, 508);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.btnAgregar);
+            this.tabPage2.Controls.Add(this.txtCorreoCliente);
+            this.tabPage2.Controls.Add(this.txtTelefonoSCliente);
+            this.tabPage2.Controls.Add(this.txtTelefonoPCliente);
+            this.tabPage2.Controls.Add(this.txtNumeroDocCliente);
+            this.tabPage2.Controls.Add(this.txtTipoDocCliente);
+            this.tabPage2.Controls.Add(this.txtApellidoCliente);
+            this.tabPage2.Controls.Add(this.txtNombreCliente);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(492, 278);
+            this.tabPage2.Size = new System.Drawing.Size(1099, 508);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // txtNombreCliente
             // 
-            this.button2.Location = new System.Drawing.Point(107, 47);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.txtNombreCliente.Location = new System.Drawing.Point(125, 52);
+            this.txtNombreCliente.Name = "txtNombreCliente";
+            this.txtNombreCliente.Size = new System.Drawing.Size(127, 22);
+            this.txtNombreCliente.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(44, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 16);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(203, 16);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Numero de Telefono Secundario";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(186, 16);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Numero de Telefono Principal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 16);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "numero Doc";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "tipo Doc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Apellido";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Nombre";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(58, 55);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 16);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Productos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Productos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtApellidoCliente
+            // 
+            this.txtApellidoCliente.Location = new System.Drawing.Point(125, 89);
+            this.txtApellidoCliente.Name = "txtApellidoCliente";
+            this.txtApellidoCliente.Size = new System.Drawing.Size(127, 22);
+            this.txtApellidoCliente.TabIndex = 24;
+            // 
+            // txtTipoDocCliente
+            // 
+            this.txtTipoDocCliente.Location = new System.Drawing.Point(125, 122);
+            this.txtTipoDocCliente.Name = "txtTipoDocCliente";
+            this.txtTipoDocCliente.Size = new System.Drawing.Size(127, 22);
+            this.txtTipoDocCliente.TabIndex = 25;
+            this.txtTipoDocCliente.Text = "DNI";
+            // 
+            // txtNumeroDocCliente
+            // 
+            this.txtNumeroDocCliente.Location = new System.Drawing.Point(128, 158);
+            this.txtNumeroDocCliente.Name = "txtNumeroDocCliente";
+            this.txtNumeroDocCliente.Size = new System.Drawing.Size(124, 22);
+            this.txtNumeroDocCliente.TabIndex = 26;
+            // 
+            // txtTelefonoPCliente
+            // 
+            this.txtTelefonoPCliente.Location = new System.Drawing.Point(240, 199);
+            this.txtTelefonoPCliente.Name = "txtTelefonoPCliente";
+            this.txtTelefonoPCliente.Size = new System.Drawing.Size(195, 22);
+            this.txtTelefonoPCliente.TabIndex = 27;
+            // 
+            // txtTelefonoSCliente
+            // 
+            this.txtTelefonoSCliente.Location = new System.Drawing.Point(240, 238);
+            this.txtTelefonoSCliente.Name = "txtTelefonoSCliente";
+            this.txtTelefonoSCliente.Size = new System.Drawing.Size(195, 22);
+            this.txtTelefonoSCliente.TabIndex = 28;
+            // 
+            // txtCorreoCliente
+            // 
+            this.txtCorreoCliente.Location = new System.Drawing.Point(92, 276);
+            this.txtCorreoCliente.Name = "txtCorreoCliente";
+            this.txtCorreoCliente.Size = new System.Drawing.Size(199, 22);
+            this.txtCorreoCliente.TabIndex = 29;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(119, 358);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(133, 37);
+            this.btnAgregar.TabIndex = 30;
+            this.btnAgregar.Text = "Agregar Cliente";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1131, 658);
-            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbc);
+            this.Controls.Add(this.btnClientes);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
+            this.tbc.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.TabControl tbc;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtNombreCliente;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCorreoCliente;
+        private System.Windows.Forms.TextBox txtTelefonoSCliente;
+        private System.Windows.Forms.TextBox txtTelefonoPCliente;
+        private System.Windows.Forms.TextBox txtNumeroDocCliente;
+        private System.Windows.Forms.TextBox txtTipoDocCliente;
+        private System.Windows.Forms.TextBox txtApellidoCliente;
+        private System.Windows.Forms.Button btnAgregar;
     }
 }
 
--- Supermercado/Form1.cs
+++ Supermercado/Form1.cs
@@ -15,14 +15,38 @@ namespace Supermercado
         public Form1()
         {
             InitializeComponent();
-            tabControl1.Appearance = TabAppearance.FlatButtons;
-            tabControl1.ItemSize = new Size(0, 1);
-            tabControl1.SizeMode = TabSizeMode.Fixed;
+            tbc.Appearance = TabAppearance.FlatButtons;
+            tbc.ItemSize = new Size(0, 1);
+            tbc.SizeMode = TabSizeMode.Fixed;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            tabControl1.SelectedTab = tabPage2;
+            tbc.SelectedTab = tabPage2;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            tbc.SelectedTab = tabPage1;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO clientes(nombre,apellido," +
+                    "tipo_doc,nro_doc,nro_tel_princ,nro_tel_sec,email)Values('" + txtNombreCliente.Text + "','" +
+                    txtApellidoCliente.Text + "','" +
+                    txtTipoDocCliente.Text + "','" + txtNumeroDocCliente.Text + "','" + txtTelefonoPCliente.Text + "','" + txtTelefonoSCliente.Text + "','" + txtCorreoCliente.Text + "')";
+            resultado = data.ExecuteQuery(query);
+            if (resultado)
+            {
+                MessageBox.Show("Registro agregado", "Sistema",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Error al agregar el registro", "Sistema",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
